7a657840d5968e5e17a3e8214ac403f03371ccee
irhamabid12 <airirham@gmail.com>
2022-09-30 18:03:22 +0700

    first commit

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Categories;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Categories>
+ */
+class CategoryFactory extends Factory
+{
+    protected $model = \App\Models\Categories::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id'   => User::factory(),
+            'name' => fake()->name()
+        ];
+    }
+}
